e82dc85fe188f40ca8aa6d166bab4f2eebd53220
do079-max <rafforicardo079@gmail.com>
2026-06-18 07:41:07 -0400

    Agrega service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'mis-horas-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE_NAME; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      if(cached) return cached;
+      return fetch(event.request).then(function(response){
+        return caches.open(CACHE_NAME).then(function(cache){
+          try{ cache.put(event.request, response.clone()); }catch(e){}
+          return response;
+        });
+      }).catch(function(){
+        return cached;
+      });
+    })
+  );
+});
